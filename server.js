3db5304acde77d0a60b53d6cdc35ef785af50544
Jonatas Viscaino <jonatasmviscaino@gmail.com>
2023-03-22 18:29:10 +0100

    back end smartbrain

---
+++ server.js
@@ -0,0 +1,38 @@
+const express = require('express');
+const bcrypt = require('bcrypt');
+const cors = require('cors');
+const db = require('knex')({
+  client: 'pg',
+  connection: {
+    host : '127.0.0.1',
+    port : 5432,
+    user : 'angieduarte',
+    password : '',
+    database : 'smart-brain'
+  }
+});
+
+const register = require('./controllers/register');
+const signin = require('./controllers/signin');
+const profile = require('./controllers/profile');
+const image = require('./controllers/image');
+
+const app = express();
+const saltRounds= 10;
+
+app.use(express.json());
+app.use(cors());
+
+app.get('/', (req, res) => {
+	res.send('sucess');
+});
+
+app.post('/signin', signin.handleSignin(db, bcrypt));
+app.post('/register', (req, res) => { register.handleRegister(req, res, db, bcrypt, saltRounds) });
+app.get('/profile/:id', (req,res) => { profile.handleProfileGet(req, res, db) });
+app.put('/image', (req,res) => { image.handleImage(req, res, db) } );
+app.post('/imageurl', (req,res) => {image.handleApiCall(req,res)}) 
+
+app.listen(3000, () => {
+	console.log('app is running on port 3000');
+});
