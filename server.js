41488bc5e19d71c12c7662e43aff8cf632f27f17
Jonatas Viscaino <jonatasmviscaino@gmail.com>
2023-03-23 15:05:25 +0100

    update server

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const cors = require('cors');
 const db = require('knex')({
   client: 'pg',
   connection: {
-    connectionString : process.env.DATABASE_URL,
+    connectionString: process.env.DATABASE_URL,
     ssl: true
   }
 });
